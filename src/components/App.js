313ad2ba9e320452af4a0708f83a86e2b75e2a9d
LuciaDuranD <oluciadurandiaz@gmail.com>
2024-01-24 11:58:02 -0500

    realizado el llamado de data, duplicada y mezclada con algoritmo de Fisher-Yates

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,72 @@
-//
-// Para incluir los diferentes sets de cartas podemos _importar_ el archivo
-// JavasSript que contenga el `export` correspondiente...
-//
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
-//
-// O alternativamente podríamos cargar el JSON de forma asíncrona usando
-// `fetch` en el momento que consideremos necesario.
-//
-// fetch('./data/pokemon/pokemon.json')
-//   .then(resp => resp.json())
-//   .then(console.log)
-//   .catch(console.error);
-//
+// DUPLICAR LAS CARTAS
+const duplicarCartas = (cartas) => {
+  // ASIGNANDO ID UNICO A CADA CARTA
+  const cartasDuplicadas = cartas.flatMap((carta) => [
+    { ...carta, id: carta.id + "_1" },
+    { ...carta, id: carta.id + "_2" },
+  ]);
 
-const App = () => {
-  const el = document.createElement('div');
-
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
-
-  return el;
+  return cartasDuplicadas;
 };
 
-export default App;
+const barajearCartas = (cartas) => {
+  //CLONAMOS ARRAY
+  const cartasBarajadas = [...cartas];
+
+  // ALGORITMO FISHER-YATES
+  for (let i = cartasBarajadas.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    // INTERCAMBIO DE POSICIONES
+    [cartasBarajadas[i], cartasBarajadas[j]] = [
+      cartasBarajadas[j],
+      cartasBarajadas[i],
+    ];
+  }
+
+  return cartasBarajadas;
+};
+
+const inicializarJuego = (cartas) => {
+  //DUPLICAR
+  const cartasDuplicadas = duplicarCartas(cartas);
+
+  //MEZCLAR
+  const cartasBarajadas = barajearCartas(cartasDuplicadas);
+  console.log("cartas barajadas:", cartasBarajadas);
+  return cartasBarajadas;
+};
+
+//RENDERIZACION
+const renderizarCartas = (cartas, contenedor) => {
+  // VERIFICO QUE "CARTAS" ESTE DEFINIDO ANTES DE ITERAR
+  if (!cartas) {
+    console.error("El conjunto de cartas es undefined");
+    return;
+  }
+
+  //LOGICA PARA RENDERIZAR LAS CARTAS DEL DOM
+  cartas.forEach((carta) => {
+    const cartaElement = document.createElement("div");
+    cartaElement.className = "carta";
+
+    // SE CREAN LAS IMAGENES CON EL LLAMADO DE LA URL
+    const imagenElement = document.createElement("img");
+    imagenElement.src = carta.image;
+    imagenElement.alt = carta.id;
+    cartaElement.appendChild(imagenElement);
+
+    // EVENTO CLIC A LA CARTA
+    cartaElement.addEventListener("click", () => manejarClicCarta(carta));
+
+    //LA CARTA EN EL CONTENEDOR
+    contenedor.appendChild(cartaElement);
+  });
+
+  console.log("cartas:", cartas);
+};
+
+const indicarVictoria = () => {
+  //PENDIENTE
+};
+
+export { inicializarJuego, renderizarCartas, indicarVictoria };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
-import App from './components/App.js';
+import {
+  inicializarJuego,
+  renderizarCartas,
+  indicarVictoria,
+} from "./components/App.js";
+import pokemon from "./data/pokemon/pokemon.js";
 
-document.getElementById('root').appendChild(App());
+const juegoContainer = document.createElement("div");
+juegoContainer.className = "juego-container";
+
+// Inicializa el juego y obtén las cartas barajadas
+const cartasBarajadas = inicializarJuego(pokemon.items);
+
+// Renderiza las cartas en el contenedor del juego
+renderizarCartas(cartasBarajadas, juegoContainer);
+
+// Agrega el contenedor del juego al elemento con id 'root' en tu HTML
+document.getElementById("root").appendChild(juegoContainer);
